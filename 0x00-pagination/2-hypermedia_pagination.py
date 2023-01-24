4be50379b81aeca9ff7beaf160f5c29ab0253595
favvie <favourabangwu@gmail.com>
2023-01-24 05:31:56 +0100

    [feat] hypermedia pagination

---
+++ 0x00-pagination/2-hypermedia_pagination.py
@@ -0,0 +1,74 @@
+#!/usr/bin/env python3
+"""hypermedia pagination"""
+import csv
+import math
+from typing import List, Dict
+
+
+def index_range(page, page_size):
+    """
+    a function that takes a page number and page size and returns
+    a tuple of a start index and an end index
+    """
+    start_index = 0
+    if page == 0:
+        start_index = 1
+        value = start_index * page_size
+    else:
+        value = page * page_size
+        for i in range(page - 1):
+            start_index = start_index + page_size
+    return (start_index, value)
+
+
+class Server:
+    """Server class to paginate a database of popular baby names.
+    """
+    DATA_FILE = "Popular_Baby_Names.csv"
+
+    def __init__(self):
+        self.__dataset = None
+
+    def dataset(self) -> List[List]:
+        """Cached dataset
+        """
+        if self.__dataset is None:
+            with open(self.DATA_FILE) as f:
+                reader = csv.reader(f)
+                dataset = [row for row in reader]
+            self.__dataset = dataset[1:]
+
+        return self.__dataset
+
+    def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
+        """return the appropriate page from a dataset"""
+        assert isinstance(page, int) and page > 0
+        assert isinstance(page_size, int) and page_size > 0
+        dataset = self.dataset()
+        index_tuple = index_range(page, page_size)
+        return dataset[index_tuple[0]:index_tuple[1]]
+
+    def get_hyper(self, page: int = 1, page_size: int = 10) -> Dict[str, int]:
+        """return a dictionary"""
+        dataset = self.dataset()
+        data = self.get_page(page, page_size)
+        total_pages = math.ceil(len(dataset) / page_size)
+
+        if self.get_page(page+1, page_size) == []:
+            next_page = None
+        else:
+            next_page = page + 1
+
+        if page == 1:
+            prev_page = None
+        elif page > 1:
+            prev_page = page - 1
+        diction = {
+            "page_size": page_size,
+            "page": page,
+            "data": data,
+            "next_page": next_page,
+            'prev_page': prev_page,
+            "total_pages": total_pages
+        }
+        return diction
