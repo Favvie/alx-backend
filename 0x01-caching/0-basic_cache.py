038747c9a3e3cf585a7eccea91f831309839c5a5
favvie <favourabangwu@gmail.com>
2023-01-26 04:17:26 +0100

    [fix] docstring update

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -1,17 +1,21 @@
 #!/usr/bin/env python3
 """basic cache"""
-import math
 BaseCaching = __import__('base_caching').BaseCaching
 
 
 class BasicCache(BaseCaching):
-    MAX_ITEMS = math.inf
+    """create basic caching system"""
+    def __init__(self):
+        """initialize the class"""
+        super().__init__()
 
     def put(self, key, item):
+        """add key to the cache"""
         if key is not None or item is not None:
             self.cache_data[key] = item
 
     def get(self, key):
+        """get item from the cache """
         cache_data = self.cache_data
         if key not in cache_data or key is None:
             return None
