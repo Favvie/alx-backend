abdc0f7ddcebfae0ac4aa261ea163978228c61d6
favvie <favourabangwu@gmail.com>
2023-04-19 00:01:15 +0100

    [feat] add method docstring fifo cache

--- 0x01-caching/1-fifo_cache.py
+++ 0x01-caching/1-fifo_cache.py
@@ -8,6 +8,7 @@ class FIFOCache(BaseCache):
         super().__init__()
 
     def put(self, key, item):
+        """add an item to the cache using a key"""
         if key is not None or item is not None:
             self.cache_data[key] = item
             if len(self.cache_data) > BaseCache.MAX_ITEMS:
@@ -16,6 +17,7 @@ class FIFOCache(BaseCache):
                 print(f"DISCARD: {first_key}\n")
 
     def get(self, key):
+        """retrieve an item from the cache using a key"""
         if key is None or key not in self.cache_data:
             return None
         return self.cache_data[key]
