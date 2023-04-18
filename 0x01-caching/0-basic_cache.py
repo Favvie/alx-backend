23f4967ec8d27c1f3b23eaf3fdd2fea2f6d1f5e3
favvie <favourabangwu@gmail.com>
2023-04-18 23:26:55 +0100

    task 0: basic cache

---
+++ 0x01-caching/0-basic_cache.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env ptyhon3
+"""basic cache module"""
+BaseCache = __import__('base_caching').BaseCaching
+
+
+class BasicCache(BaseCache):
+    """Basic cache class that inherits from BaseCache"""
+
+    def put(self, key, item):
+        """add an item to the cache using a key"""
+        if key is not None or item is not None:
+            self.cache_data[key] = item
+
+    def get(self, key):
+        """retrieve an item from the cache using a key"""
+        if key is None or key not in self.cache_data:
+            return None
+        return self.cache_data[key]
