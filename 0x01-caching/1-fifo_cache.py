b18c3aa556c9fd7de019e700224f338fe431f47a
favvie <favourabangwu@gmail.com>
2023-04-19 00:00:03 +0100

    task 1: fifo cache algorithm

---
+++ 0x01-caching/1-fifo_cache.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+"""Fifo caching algorithm module"""
+BaseCache = __import__('base_caching').BaseCaching
+
+
+class FIFOCache(BaseCache):
+    def __init__(self):
+        super().__init__()
+
+    def put(self, key, item):
+        if key is not None or item is not None:
+            self.cache_data[key] = item
+            if len(self.cache_data) > BaseCache.MAX_ITEMS:
+                first_key = list(self.cache_data.keys())[0]
+                del self.cache_data[first_key]
+                print(f"DISCARD: {first_key}\n")
+
+    def get(self, key):
+        if key is None or key not in self.cache_data:
+            return None
+        return self.cache_data[key]
