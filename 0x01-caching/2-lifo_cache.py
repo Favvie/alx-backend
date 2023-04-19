291916cdf92bac1366bd51a6694332671c57a762
favvie <favourabangwu@gmail.com>
2023-04-19 10:38:42 +0100

    task 2: lifo cache algortihm

---
+++ 0x01-caching/2-lifo_cache.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+"""LIFO caching algorithm module"""
+BaseCache = __import__('base_caching').BaseCaching
+
+
+class LIFOCache(BaseCache):
+    def __init__(self) -> None:
+        super().__init__()
+
+    def put(self, key, item):
+        """Add item to cache"""
+        if key is not None or item is not None:
+            if key in self.cache_data.keys():
+                self.cache_data.pop(key)
+            self.cache_data[key] = item
+
+            if len(self.cache_data) > self.MAX_ITEMS:
+                lastItemBeforeInsert = list(self.cache_data.keys())[-2]
+                self.cache_data.pop(lastItemBeforeInsert)
+                print(f'DISCARD: {lastItemBeforeInsert}')
+
+    def get(self, key):
+        """Get item from cache"""
+        if key is None or key not in self.cache_data:
+            return None
+        return self.cache_data[key]
